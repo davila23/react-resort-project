e28d1c3c8f31b5494ee524970beb3dd61a00a895
Daniel <daniel.avila@rottay.com>
2020-03-21 20:06:12 -0300

    init

---
+++ src/Contentful.js
@@ -0,0 +1,6 @@
+const contentful = require("contentful");
+
+export default contentful.createClient({
+  space: "48t1s0p1dk0p",
+  accessToken: "61dda3b65d92ab91decdcde3f89f5d268746742a792e27b9981a33a1a76c8c7f"
+});
